918e288bb80605152f5eb24252b95748c7378afd
Darlan Marangoni <marangoni.darlan1@gmail.com>
2025-07-09 15:48:10 -0300

    S2-A31 - Widget tree

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 
-main() {
-  runApp(PerguntaApp());
-}
+main() => runApp(PerguntaApp());
 
 class PerguntaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Helllo Flutter!!!'));
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Perguntas')),
+        body: Text('Ola Flutter'),
+      ),
+    );
   }
 }
