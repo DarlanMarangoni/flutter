df7c64b88da44ab90cc3d939dc26718873356838
Darlan Marangoni <marangoni.darlan1@gmail.com>
2025-07-09 16:05:51 -0300

    S2-A34 - Add column

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -5,10 +5,21 @@ main() => runApp(PerguntaApp());
 class PerguntaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      'Qual a sua cor favorita?',
+      'Qual o seu animal favorito?',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('Perguntas')),
-        body: Text('Ola Flutter'),
+        body: Column(
+          children: [
+            Text(perguntas.elementAt(0)),
+            ElevatedButton(onPressed: null, child: Text('Resposta 1')),
+            ElevatedButton(onPressed: null, child: Text('Resposta 2')),
+            ElevatedButton(onPressed: null, child: Text('Resposta 3')),
+          ],
+        ),
       ),
     );
   }
