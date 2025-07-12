4eb29222f2933744b335b02448fe4aff27bb20ff
Darlan Marangoni <marangoni.darlan1@gmail.com>
2025-07-12 12:50:02 -0300

    S2 - Create congratulations widget

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -6,48 +6,59 @@ main() => runApp(PerguntaApp());
 
 class _PerguntaAppState extends State<PerguntaApp> {
   var _perguntaSelecionada = 0;
+  final _perguntas = const [
+    {
+      'texto': 'Qual a sua cor favorita?',
+      'respostas': ['Preto', 'Vermelho', 'Verde', 'Branco'],
+    },
+    {
+      'texto': 'Qual o seu animal favorito?',
+      'respostas': ['Coellho', 'Cobra', 'Elefante', 'Leao'],
+    },
+    {
+      'texto': 'Qual o seu instrutor favorito?',
+      'respostas': ['Maria', 'Joao', 'Leo', 'Pedro'],
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
-    final List<Map<String, Object>> perguntas = [
-      {
-        'texto': 'Qual a sua cor favorita?',
-        'respostas': ['Preto', 'Vermelho', 'Verde', 'Branco'],
-      },
-      {
-        'texto': 'Qual o seu animal favorito?',
-        'respostas': ['Coellho', 'Cobra', 'Elefante', 'Leao'],
-      },
-      {
-        'texto': 'Qual o seu instrutor favorito?',
-        'respostas': ['Maria', 'Joao', 'Leo', 'Pedro'],
-      },
-    ];
-    final List<Widget> respostas =
-        (perguntas.elementAt(_perguntaSelecionada)['respostas'] as List<String>)
-            .map((item) => Resposta(item, _responder))
-            .toList();
+    final List<Widget> respostas = temPerguntaSelecionada
+        ? (_perguntas.elementAt(_perguntaSelecionada)['respostas']
+                  as List<String>)
+              .map((item) => Resposta(item, _responder))
+              .toList()
+        : [];
 
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('Perguntas')),
-        body: Column(
-          children: [
-            Questao(
-              perguntas.elementAt(_perguntaSelecionada)['texto'] as String,
-            ),
-            ...respostas,
-          ],
-        ),
+        body: temPerguntaSelecionada
+            ? Column(
+                children: [
+                  Questao(
+                    _perguntas.elementAt(_perguntaSelecionada)['texto']
+                        as String,
+                  ),
+                  ...respostas,
+                ],
+              )
+            : Center(child: Text('Parabens!', style: TextStyle(fontSize: 28))),
       ),
     );
   }
 
+  bool get temPerguntaSelecionada {
+    return _perguntaSelecionada < _perguntas.length;
+  }
+
   void _responder() {
-    setState(() {
-      _perguntaSelecionada++;
-    });
-    print('Pergunta respondida');
+    if (temPerguntaSelecionada) {
+      setState(() {
+        _perguntaSelecionada++;
+      });
+      print('Pergunta respondida');
+    }
   }
 }
 
