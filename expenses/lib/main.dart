908c42509647fd19c11e098ca2c47fb4085d93b6
Darlan Marangoni <marangoni.darlan1@gmail.com>
2025-08-27 20:55:05 -0300

    94 - Desafio Estilo Titulo e Data

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -69,7 +69,20 @@ class MyHomePage extends StatelessWidget {
                       ),
                     ),
                     Column(
-                      children: [Text(tr.title), Text(tr.date.toString())],
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          tr.title,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                        Text(
+                          tr.date.toString(),
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ],
                     ),
                   ],
                 ),
