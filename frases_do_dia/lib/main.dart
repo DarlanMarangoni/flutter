c1e34b6cfddb14b9b662519b109f1379c4d5a7e2
Darlan Marangoni <marangoni.darlan1@gmail.com>
2026-01-15 19:36:14 -0300

    60: Alinhamento

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -12,18 +12,15 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       title: "Frases do Dia",
       home: Container(
-        padding: EdgeInsets.fromLTRB(10, 30, 10, 0),
-        margin: EdgeInsets.all(30),
+        margin: EdgeInsets.only(top: 80),
         decoration: BoxDecoration(
           border: Border.all(width: 3, color: Colors.white),
           borderRadius: BorderRadius.circular(12),
         ),
         child: Column(
-          children: [
-            Text("t1"),
-            Padding(padding: EdgeInsets.all(30), child: Text("t2")),
-            Text("t3"),
-          ],
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [Text("t1"), Text("t2"), Text("t3")],
         ),
       ),
     );
